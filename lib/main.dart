c5a851b50d44edbb5292f9c5ee5fe1b1dd5eaf0c
Kariithi <iankariithi@gmail.com>
2026-07-23 17:53:10 +0300

    Added gen_runner and native_splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:gigglify_rp/di.dart';
 import 'package:gigglify_rp/presentation/app.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final WidgetsBinding binding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: binding);
 
   if (kReleaseMode) {
     debugPrint = (String? message, {int? wrapWidth}) {};
